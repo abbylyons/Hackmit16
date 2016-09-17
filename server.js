e292525deda3cfcdf7dc5cf0b304801974b3b08d
csun01 <Scott@Scotts-MacBook-Pro-2.local>
2016-09-17 17:02:54 -0400

    server parsing JSON wrong

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
 // set our port
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8080;
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json
 app.use(bodyParser.json());
-
-app.use(favicon(__dirname + '/dist/img/hurclogo.png'));
 
 // parse application/vnd.api+json as json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
@@ -22,9 +20,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override'));
-
-// set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/dist'));
 
 // routes ==================================================
 require('./app/routes')(app); // configure our routes
