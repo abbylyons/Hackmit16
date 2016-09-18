2e64d0f6ae35bd931fd3c649d87e1a00fc0e171e
Abby <abigaillyons@college.harvard.edu>
2016-09-17 23:48:01 -0400

    stuff

--- app/article_search.py
+++ app/article_search.py
@@ -6,50 +6,42 @@ import requests
 from datetime import date
 from datetime import timedelta
 
+start = unicode(date.today() + timedelta(-2))
+end = unicode(date.today())
+
 # Get articles for Shillary
 er = EventRegistry()
-q = QueryArticles(lang=["eng"])
+q = QueryArticles(lang=["eng"], dateStart = unicode(start), dateEnd = unicode(end))
 q.addConcept(er.getConceptUri("Hillary Clinton"))   
-q.addRequestedResult(RequestArticlesInfo(sortBy = "date", count=10))   # return event details for last 100 events
+q.addRequestedResult(RequestArticlesInfo(count=200))
 results = er.execQuery(q)
-
-#for result in results:
-#	url = result["url"]
-#	r = requests.get(url)
-#	data = r.text
-#	print data
-
 
 # Get articles for Frump
 er = EventRegistry()
-q = QueryArticles(lang=["eng"])
+q = QueryArticles(lang=["eng"], dateStart = unicode(start), dateEnd = unicode(end))
 q.addConcept(er.getConceptUri("Donald Trump"))   
-q.addRequestedResult(RequestArticlesInfo(sortBy = "date", count=10))   # return event details for last 100 events
-print er.execQuery(q)
-
+q.addRequestedResult(RequestArticlesInfo(count=200))
+results = er.execQuery(q)
 
 ####################
 
-start = unicode(date.today() + timedelta(-30))
-end = unicode(date.today())
-
 # Determine occurrence of Hillary or Trump in news of period of time
-er = EventRegistry()
-q = GetCounts(er.getConceptUri("Clinton"),
-              source = "news",
-              startDate = start, endDate = end)
-print er.execQuery(q)
+# er = EventRegistry()
+# q = GetCounts(er.getConceptUri("Clinton"),
+#               source = "news",
+#               startDate = start, endDate = end)
+# print er.execQuery(q)
 
-# Determine occurrence of Hillary or Trump in news of period of time
-er = EventRegistry()
-q = GetCounts(er.getConceptUri("Trump"),
-              source = "news",
-              startDate = start, endDate = end)
-print er.execQuery(q)
+# # Determine occurrence of Hillary or Trump in news of period of time
+# er = EventRegistry()
+# q = GetCounts(er.getConceptUri("Trump"),
+#               source = "news",
+#               startDate = start, endDate = end)
+# print er.execQuery(q)
 
-# Get most recent articles
-er = EventRegistry()
-q = QueryArticles(lang=["eng"], dateStart = unicode(start), dateEnd = unicode(end))
-q.addRequestedResult(RequestArticlesInfo(count=50))   # return event details for last 100 events
-results = er.execQuery(q)
-print results
+# # Get most recent articles
+# er = EventRegistry()
+# q = QueryArticles(lang=["eng"], dateStart = unicode(start), dateEnd = unicode(end))
+# q.addRequestedResult(RequestArticlesInfo(count=50))   # return event details for last 100 events
+# results = er.execQuery(q)
+# print results
