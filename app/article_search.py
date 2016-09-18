8c56138378517243a91e6060066bff47a112eaf7
Abby <abigaillyons@college.harvard.edu>
2016-09-17 20:54:13 -0400

    added preliminary results

--- app/article_search.py
+++ app/article_search.py
@@ -1,8 +1,26 @@
 import eventregistry
-
 from eventregistry import *
+from bs4 import BeautifulSoup
+
+import requests
+
+# Get articles for Shillary
 er = EventRegistry()
-q = QueryEvents()
+q = QueryArticles(lang=["eng"])
 q.addConcept(er.getConceptUri("Hillary Clinton"))   
-q.addRequestedResult(RequestEventsInfo(sortBy = "date", count=100))   # return event details for last 100 events
+q.addRequestedResult(RequestArticlesInfo(sortBy = "date", count=10))   # return event details for last 100 events
+results = er.execQuery(q)
+
+for result in results:
+	url = result["url"]
+	r = requests.get(url)
+	data = r.text
+	print data
+
+
+# Get articles for Frump
+er = EventRegistry()
+q = QueryArticles(lang=["eng"])
+q.addConcept(er.getConceptUri("Donald Trump"))   
+q.addRequestedResult(RequestArticlesInfo(sortBy = "date", count=10))   # return event details for last 100 events
 print er.execQuery(q)
