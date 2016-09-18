24ce53edb0bddea32e478952e5d6401096a5d910
Abby <abigaillyons@college.harvard.edu>
2016-09-17 23:56:33 -0400

    stuff is here kind of

--- app/article_search.py
+++ app/article_search.py
@@ -6,22 +6,51 @@ import requests
 from datetime import date
 from datetime import timedelta
 
+##### Hillary/Trump/Query related articles #####
+
+def visible(element):
+    if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+        return False
+    elif re.match('<!--.*-->', str(element)):
+        return False
+    return True
+
 start = unicode(date.today() + timedelta(-2))
 end = unicode(date.today())
 
 # Get articles for Shillary
-er = EventRegistry()
-q = QueryArticles(lang=["eng"], dateStart = unicode(start), dateEnd = unicode(end))
-q.addConcept(er.getConceptUri("Hillary Clinton"))   
-q.addRequestedResult(RequestArticlesInfo(count=200))
-results = er.execQuery(q)
+def hillaryArticles(query):
+	er = EventRegistry()
+	q = QueryArticles(lang=["eng"], dateStart = unicode(start), dateEnd = unicode(end))
+	q.addConcept(er.getConceptUri("Hillary Clinton"))   
+	q.addRequestedResult(RequestArticlesInfo(count=200))
+	results = er.execQuery(q)
+	for result in results:
+		url = result["url"]
+		r = urllib.urlopen(url).read()
+		soup = BeautifulSoup(r)
+		texts = soup.findAll(text=True)
+		visible_texts = filter(visible, texts)
+		return visible_texts
 
 # Get articles for Frump
-er = EventRegistry()
-q = QueryArticles(lang=["eng"], dateStart = unicode(start), dateEnd = unicode(end))
-q.addConcept(er.getConceptUri("Donald Trump"))   
-q.addRequestedResult(RequestArticlesInfo(count=200))
-results = er.execQuery(q)
+def hillaryArticles(query):
+	er = EventRegistry()
+	q = QueryArticles(lang=["eng"], dateStart = unicode(start), dateEnd = unicode(end))
+	q.addConcept(er.getConceptUri("Donald Trump"))   
+	q.addRequestedResult(RequestArticlesInfo(count=200))
+	results = er.execQuery(q)
+	for result in results:
+		url = result["url"]
+		r = urllib.urlopen(url).read()
+		soup = BeautifulSoup(r)
+		texts = soup.findAll(text=True)
+		visible_texts = filter(visible, texts)
+		return visible_texts
+
+if __name__ == "__main__":
+    print hillaryArticles("girl scout")
+    print trumpArticles("girl scout")
 
 ####################
 
