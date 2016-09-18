97a14d37918fd3ec097c73507bc080fe2069f418
csun01 <Scott@Scotts-MacBook-Pro-2.local>
2016-09-17 22:10:47 -0400

    more search

--- app/article_search.py
+++ app/article_search.py
@@ -11,11 +11,11 @@ q.addConcept(er.getConceptUri("Hillary Clinton"))
 q.addRequestedResult(RequestArticlesInfo(sortBy = "date", count=10))   # return event details for last 100 events
 results = er.execQuery(q)
 
-for result in results:
-	url = result["url"]
-	r = requests.get(url)
-	data = r.text
-	print data
+#for result in results:
+#	url = result["url"]
+#	r = requests.get(url)
+#	data = r.text
+#	print data
 
 
 # Get articles for Frump
@@ -23,4 +23,30 @@ er = EventRegistry()
 q = QueryArticles(lang=["eng"])
 q.addConcept(er.getConceptUri("Donald Trump"))   
 q.addRequestedResult(RequestArticlesInfo(sortBy = "date", count=10))   # return event details for last 100 events
+print er.execQuery(q)
+
+start = datetime.date.today()+datetime.timedelta(-30)
+end = datetime.date.today()
+
+# Determine occurrence of Hillary or Trump in news of period of time
+er = EventRegistry()
+q = GetCounts(er.getConceptUri("Clinton"),
+              source = "news",
+              startDate = start, endDate = end)
+print er.execQuery(q)
+
+start = datetime.date.today()+datetime.timedelta(-30)
+end = datetime.date.today()
+
+# Determine occurrence of Hillary or Trump in news of period of time
+er = EventRegistry()
+q = GetCounts(er.getConceptUri("Trump"),
+              source = "news",
+              startDate = start, endDate = end)
+print er.execQuery(q)
+
+# Get most recent articles
+er = EventRegistry()
+q = QueryArticles(lang=["eng"])
+q.addRequestedResult(RequestArticlesInfo(startDate = start, endDate = end, count=50))   # return event details for last 100 events
 print er.execQuery(q)
